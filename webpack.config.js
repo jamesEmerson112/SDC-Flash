5eaf1651ce5076254930faca161b5ca62446ca19
Brian Pham <phambri97@gmail.com>
2022-08-24 11:21:21 -0700

    Adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "client/src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "client/dist"),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  }
+}
