b4c82458c13070dd7d620b30ee35ea826a188bdb
Blake Lenhard <lenhardbc@gmail.com>
2022-08-25 09:30:23 -0700

    ting useEffect, deconstructed react dependencies.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import config from '../../../env/config.js';
 
 const App = () => {
-  const [product, setProduct] = React.useState({});
+  const [product, setProduct] = useState({});
 
   // will set product to the first product in list
   // maybe set this up to be random later
-  React.useEffect(() => {
-    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products', config)
+  useEffect(() => {
+    axios.get('/products', config)
     .then((response) => {
       setProduct(response.data[0]);
     })
