aab4c752a5269cba0800cc9ff5a12f250577fa18
Brian Pham <phambri97@gmail.com>
2022-08-25 10:55:14 -0700

    Adding component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import config from '../../../env/config.js';
+import React, { useState, useEffect } from "react";
+import Ratings_Reviews from "./Ratings_Reviews.jsx";
+import axios from "axios";
+import config from "../../../env/config.js";
 
 const App = () => {
   const [product, setProduct] = useState({});
@@ -8,18 +9,22 @@ const App = () => {
   // will set product to the first product in list
   // maybe set this up to be random later
   useEffect(() => {
-    axios.get('/products', config)
-    .then((response) => {
-      setProduct(response.data[0]);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  },[])
+    axios
+      .get("/products", config)
+      .then((response) => {
+        setProduct(response.data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
-    <h1>{product.id}: this is the product id that we can pass to each component</h1>
-  )
-}
+    <h1>
+      {product.id}: this is the product id that we can pass to each component
+      <Ratings_Reviews />
+    </h1>
+  );
+};
 
 export default App;
