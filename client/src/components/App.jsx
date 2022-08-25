8d6eeb52a3f122bbe18b5918f5ec10754714ca8c
Blake Lenhard <lenhardbc@gmail.com>
2022-08-24 19:37:35 -0700

    Initialized the state for App.jsx using axios get request with preset header. Set it to the first product in list of products returned by the API. Created an env directory, and skeleton for setting up your API token.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import axios from 'axios';
+import config from '../../../env/config.js';
 
 const App = () => {
+  const [product, setProduct] = React.useState({});
+
+  // will set product to the first product in list
+  // maybe set this up to be random later
+  React.useEffect(() => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products', config)
+    .then((response) => {
+      setProduct(response.data[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  },[])
+
   return (
-    <h1>Yeet</h1>
+    <h1>{product.id}: this is the product id that we can pass to each component</h1>
   )
 }
 
