ead48b235d5751242451a6c802d0180f90090882
Timothy Loo <TimothyLoo07@gmail.com>
2022-08-26 06:24:00 -1000

    added component framework for Q&A section

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import config from "../../../env/config.js";
-import QuestionList from "./QuestionList.jsx";
+import QuestionList from "./QandA/QuestionList.jsx";
 import Ratings_Reviews from "./Ratings_Reviews.jsx";
 import ProductOverview from "./ProductOverview.jsx";
 
@@ -14,24 +14,28 @@ const App = () => {
     axios
       .get("/products", config)
       .then((response) => {
-        setProduct(response.data[0]);
+        setProduct(response.data[1]);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  return (
-    <div>
-      <h1>
-        {product.id}: this is the product id that we can pass to each component
-      </h1>
-      <QuestionList />
-      <Ratings_Reviews />
-      <ProductOverview />
-
-    </div>
-  );
+  if ("id" in product) {
+    return (
+      <div>
+        <h1>
+          {product.id}: this is the product id that we can pass to each
+          component
+        </h1>
+        <QuestionList product={product} />
+        <Ratings_Reviews />
+        <ProductOverview />
+      </div>
+    );
+  } else {
+    return <div>loading</div>;
+  }
 };
 
 export default App;
--- client/src/components/QuestionList.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function QuestionList() {
-  return <div>I am a question list component</div>;
-}
