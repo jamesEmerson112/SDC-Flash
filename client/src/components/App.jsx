84d9fbeb0860afa13a7125ba6b988d985e0925c5
Brian Pham <phambri97@gmail.com>
2022-08-29 10:31:18 -0700

    Added Ratings Modal (in the making)

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 import config from "../../../env/config.js";
 import QuestionList from "./QandA/QuestionList.jsx";
 import RRIndex from "./Ratings_Reviews/index.jsx";
-import ProductOverview from "./ProductOverview.jsx";
+import ProductOverview from "./ProductOverview/ProductOverview.jsx";
 
 const App = () => {
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState();
 
   // will set product to the first product in list
   // maybe set this up to be random later
@@ -14,28 +14,23 @@ const App = () => {
     axios
       .get("/products", config)
       .then((response) => {
-        setProduct(response.data[1]);
+        setProduct(response.data[0].id);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  if ("id" in product) {
-    return (
-      <div>
-        <h1>
-          {product.id}: this is the product id that we can pass to each
-          component
-        </h1>
-        <QuestionList product={product} />
-        <RRIndex id={product.id} />
-        <ProductOverview id={product.id} />
-      </div>
-    );
-  } else {
-    return <div>loading</div>;
-  }
+  return (
+    <div>
+      <h1>
+        {product}: this is the product id that we can pass to each component
+      </h1>
+      {/* <QuestionList product={product} />
+        <ProductOverview id={product.id} /> */}
+      <RRIndex id={product} />
+    </div>
+  );
 };
 
 export default App;
