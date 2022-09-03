a7d267c241bfb56642ee785bb38f8524faf4750f
Timothy Loo <TimothyLoo07@gmail.com>
2022-09-03 07:21:54 -1000

    clean up test file

--- client/src/components/QandA/__QAtests__/QuestionList.test.js
+++ client/src/components/QandA/__QAtests__/QuestionList.test.js
@@ -1,16 +1,6 @@
 import React from "react";
-// import { act } from "react-dom/test-utils";
-// import { render, screen, cleanup } from "@testing-library/react";
 import renderer, { act } from "react-test-renderer";
 import QuestionList from "../Questionlist.jsx";
-
-beforeAll((done) => {
-  done();
-});
-
-afterAll((done) => {
-  done();
-});
 
 test("should render QuestionList component", () => {
   const tree = renderer.create(<QuestionList product={{ id: 65631 }} />);
