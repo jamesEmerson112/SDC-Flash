7f424b4b6d8122d11024e50c0d9528bb73810828
Blake Lenhard <lenhardbc@gmail.com>
2022-09-07 15:13:45 -0700

    oops edited master

--- env/config copy.js
+++ env/config copy.js
@@ -1,13 +1,12 @@
 // Create a copy and rename file to config.js
 // on the gitignore list
-// add your API token
+// add your API tok
 
 module.exports = {
   config: {
     baseURL: "https://app-hrsei-api.herokuapp.com/api/fec2/rfp",
     headers: {
-      authorization: "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",
+      authorization: API_Token,
     },
-  }
+  },
 };
-
