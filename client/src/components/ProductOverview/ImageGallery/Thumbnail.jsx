f30bc7d551b083ad4ca6b02b5081228ec76a7c94
Blake Lenhard <lenhardbc@gmail.com>
2022-09-08 16:02:52 -0700

    fix problem for will appearing on error of Model lode for thumbnail

--- client/src/components/ProductOverview/ImageGallery/Thumbnail.jsx
+++ client/src/components/ProductOverview/ImageGallery/Thumbnail.jsx
@@ -26,7 +26,15 @@ const Thumbnail = ({ photos, click, selected, setSelected, modal }) => {
           key={i}
           className={selected === i ? "selected" : "not_selected"}
         >
-          <img className="thumbnail" src={photo.thumbnail_url} />
+          <img
+            onError={(e) => {
+              e.target.src =
+                "https://www.cnet.com/a/img/resize/905e1d3662ccaaf4763408156c833b91a47dfd07/2020/08/31/9562c49a-8f37-434d-8070-2751fb03d683/will-smith-fresh-prince-bel-air.jpg?auto=webp&fit=crop&height=900&width=1200";
+              e.target.onError = null;
+            }}
+            className="thumbnail"
+            src={photo.thumbnail_url}
+          />
         </ThumbnailParent>
       );
     });
@@ -55,7 +63,15 @@ const Thumbnail = ({ photos, click, selected, setSelected, modal }) => {
             }}
             key={i}
           >
-            <ModalThumbnail src={photo.thumbnail_url} width="80px" />
+            <ModalThumbnail
+              onError={(e) => {
+                e.target.src =
+                  "https://www.cnet.com/a/img/resize/905e1d3662ccaaf4763408156c833b91a47dfd07/2020/08/31/9562c49a-8f37-434d-8070-2751fb03d683/will-smith-fresh-prince-bel-air.jpg?auto=webp&fit=crop&height=900&width=1200";
+                e.target.onError = null;
+              }}
+              src={photo.thumbnail_url}
+              width="80px"
+            />
           </ModalParentNotSelected>
         );
       }
