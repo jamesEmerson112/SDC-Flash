65dc276ee0e30785dc246d120d9194be5aa94b86
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-09-16 17:50:30 -0700

    test

--- server/Database/db.js
+++ server/Database/db.js
@@ -9,6 +9,7 @@ const productFile = path.resolve(__dirname, './product.csv');
 let stream = fs.createReadStream(productFile);
 let csvData = [];
 
+<<<<<<< Updated upstream
 // // config for Pool
 // const config = {
 //   host: process.env.LOCAHOST,
@@ -52,6 +53,51 @@ let csvData = [];
 
 
 // stream.pipe(csvStream);
+=======
+// config for Pool
+const config = {
+  host: process.env.LOCAHOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT_DB,
+}
+
+let csvStream = fastcsv
+              .parse()
+              .on('data', (data) => {
+                // console.log(data);
+                csvData.push(data);
+              })
+              .on('end', () => {
+                csvData.shift();
+
+                // connecting to postgresSQL with Pool
+
+                const pool = new Pool(config);
+
+                // Transform and Load
+                const query =
+                    'INSERT INTO category (id,name,slogan,description,category,default_price) VALUES ($1, $2, $3, $4, $5, $6)';
+
+                pool.connect().then(client => {
+                  csvData.forEach((row) => {
+                    client.query(query, ['pg-pool'])
+                      .then(res => {
+                        client.release();
+                        console.log('hello from ', res.rows[0]);
+                      })
+                      .catch(e => {
+                        client.release();
+                        console.error('query error ', e.message, e.stack);
+                      })
+                  })
+                })
+              })
+
+
+stream.pipe(csvStream);
+>>>>>>> Stashed changes
 
 
 // const test = 'test';
