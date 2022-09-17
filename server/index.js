687f497059aae433df9827fd4744df5b4d2f4883
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-09-17 10:06:24 -0700

    adding routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const V = require('./Utility/utility.js');
 const database = require('./Database/db.js');
 
+// Router
+var router = require('./routes.js');
+
+var db = require('./Database');
+
+// Middleware
+var morgan = require('morgan');
+var cors = require('cors');
+
+// logging and parsing
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -14,4 +28,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 console.log(`Listening at http://localhost:${PORT}`);
 
+// Set up our routes
+// switching to localhost:3000 perhaps?
+app.use('https://localhost:3000/api/fec2/rfp', router);
+
+
 // ==============DATABASE================
+
