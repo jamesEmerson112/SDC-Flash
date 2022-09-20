bac3b3393f3a57f010d261969c80092824a88397
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-09-20 00:10:04 -0700

    Update index.js
    Refactoring the ETL features

--- server/ETL/index.js
+++ server/ETL/index.js
@@ -14,45 +14,74 @@ const config = {
 const pool = new Pool(config);
 const productFile = path.resolve(__dirname, '../Data/product.csv');
 
+const createTable = (tableName) => {
+  return `CREATE TABLE IF NOT EXISTS "${tableName}" `;
+};
+
 const runQuery = async () => {
-  const client = await pool.connect();
-  // client.query()
-  //     .then((res) => {
-  //       pool.query(`COPY persons(id,name,slogan,description
-  //         ,category,default_price)
-  //         FROM '${productFile}
-  //         DELIMITER ','
-  //         CSV HEADER;`);
-  //     });
-  const queryStr = `CREATE TABLE IF NOT EXISTS "Product1" (id SERIAL,
-    name VARCHAR(500),
-    slogan VARCHAR(500), description text, category VARCHAR(500),
-    default_price DOUBLE PRECISION, PRIMARY KEY(id))`;
-  const copyProduct = `COPY Product1 (id,name,slogan,description
-             ,category,default_price)
-             FROM '${productFile}'
-             DELIMITER ','
-             CSV HEADER;`;
-  client.query(queryStr)
-      .then((data) => {
-        // data = data.rows || {};
-        client.query(copyProduct)
-            .then(() => {
-              console.log('done');
-            })
-            .catch((err) => {
-              console.log(err.message);
-            });
-      })
-      .catch((err) => {
-        console.log('Error connection');
-      });
-  // client.query(`GRANT pg_read_server_files TO ${config.user};`)
-  //     .then(() => {
-  //       console.log('GRANT works');
-  //     });
-  client.release();
-  // console.log('testing ETL');
+  // const queryStr = createTable('Product1') + `(id SERIAL,
+  //   name VARCHAR(500),
+  //   slogan VARCHAR(500), description text, category VARCHAR(500),
+  //   default_price DOUBLE PRECISION, PRIMARY KEY(id))`;
+
+  // // table name + columns
+  // const copyProduct = `COPY "Product1" (id,name,slogan,description
+  //            ,category,default_price)
+  //            FROM '${productFile}'
+  //            DELIMITER ','
+  //            CSV HEADER;`;
+  const copyTables = async (tableName,
+      columnNameType,
+      columnName,
+      csvFile) => {
+    const client = await pool.connect();
+
+    // table name + columns (and column types)
+    const queryStr = createTable(tableName) + columnNameType;
+
+    // table name + columns
+    const copyProduct = `COPY "${tableName}" ${columnName}
+               FROM '${csvFile}'
+               DELIMITER ','
+               CSV HEADER;`;
+    client.query(queryStr)
+        .then((data) => {
+          // data = data.rows || {};
+          client.query(copyProduct)
+              .then(() => {
+                console.log('done');
+              })
+              .catch((err) => {
+                console.log(err.message);
+              });
+        })
+        .catch((err) => {
+          console.log('Error connection');
+        });
+    client.release();
+  };
+
+  // copyTables('Product1',
+  //     `(id SERIAL,
+  //   name VARCHAR(500),
+  //   slogan VARCHAR(500), description text, category VARCHAR(500),
+  //   default_price DOUBLE PRECISION, PRIMARY KEY(id))`,
+  //     `(id,name,slogan,description
+  //     ,category,default_price)`,
+  //     productFile);
+
+  // copyTables('Related1',
+  //     `id,
+  // current_product_id,
+  // related_product_id`,
+  //     `(id,current_product_id,related_product_id)`);
+
+  // copyTables('Styles1',
+  //     `(id,productId,name,sale_price,original_price,default_style)`,
+  //     `(id,productId,name,sale_price,original_price,default_style)`);
+
+  // copyTables('Features1', `(id,product_id,feature,value)`,
+  // `(id,product_id,feature,value)`);
 };
 
 // runQuery();
